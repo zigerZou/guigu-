d3f811f796b5e31b2175b97fe8ab9dc4014f186d
test <3087303520@qq.com>
2023-12-13 16:20:00 +0800

    chore:添加配置eslint,prettier,commitlint

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <h2>hello world</h2>
+  <div class="box">
+    <p>哈里路大旋风</p>
+  </div>
 </template>
 
 <script>
-
+const str = 'i love american'
+for (let i = 1; i < 10; i++) console.log(i * i)
 </script>
 
 <style>
-
+.box {
+  height: 400px;
+  width: 300px;
+  color: pink;
+}
 </style>
